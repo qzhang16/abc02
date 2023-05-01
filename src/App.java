c0e8a9f548978e00ffa3f472632146c862aeb811
qzhang16 <qzhang16@ra.rockwell.com>
2023-05-01 20:35:22 +0800

    update App.java to read and print the test.out file.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Path path = Paths.get("test.out");
+        Scanner scanner = new Scanner(Files.newBufferedReader(path));
+        String str;
+        while (scanner.hasNextLine()) {
+            str = scanner.nextLine();
+            System.out.println(str);
+        }
+        scanner.close();
     }
 }
