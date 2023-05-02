a3b17ac45ff9aa2e46d497f056be48fbb66b6b3d
qzhang16 <qzhang16@ra.rockwell.com>
2023-05-02 17:01:51 +0800

    add Demo01

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -15,5 +17,84 @@ public class App {
             System.out.println(str);
         }
         scanner.close();
+
+        Demo01 d01 = new Demo01("Jack");
+        Demo01 d02 = new Demo01("Tom");
+        d01.addFriend(d02);
+
+        System.out.println("#".repeat(50));
+        System.out.println(d01);
+        System.out.println("#".repeat(50));
+        System.out.println(d02);
+
+
+        
+
+
+    }
+
+    private static class Demo01 {
+        private String name;
+        private List<Demo01> friends;
+
+        public Demo01(String name) {
+            this.name = name;
+            friends = new ArrayList<Demo01>();
+        }
+
+        public boolean addFriend(Demo01 demo01) {
+            if (friends.contains(demo01)) {
+                return false;
+            } else {
+                friends.add(demo01);
+                return true;
+            }
+
+        }
+
+        
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((name == null) ? 0 : name.hashCode());
+            result = prime * result + ((friends == null) ? 0 : friends.hashCode());
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            // if (this == obj)
+            //     return true;
+            // if (obj == null)
+            //     return false;
+            // if (getClass() != obj.getClass())
+            //     return false;
+            // Demo01 other = (Demo01) obj;
+            // if (name == null) {
+            //     if (other.name != null)
+            //         return false;
+            // } else if (!name.equals(other.name))
+            //     return false;
+            // if (friends == null) {
+            //     if (other.friends != null)
+            //         return false;
+            // } else if (!friends.equals(other.friends))
+            //     return false;
+            // return true;
+            return name.equals(((Demo01) obj).getName());
+        }
+
+        @Override
+        public String toString() {
+            return "Demo01 [name=" + name + ", friends=" + friends + "]";
+        }
+        
+        
     }
 }
