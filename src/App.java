2336147b3296439c51e42bf4f8841fc758e72b89
qzhang16 <qzhang16@ra.rockwell.com>
2023-05-02 11:15:48 +0800

    revert the # print change

--- src/App.java
+++ src/App.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("#".repeat(100));
         Path path = Paths.get("test.out");
         Scanner scanner = new Scanner(Files.newBufferedReader(path));
         String str;
